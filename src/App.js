f1e04235525f43a641d895fdf1a68bcbe3cbe130
Dino Tudor <dinotudor@gmail.com>
2019-08-08 17:46:00 +0200

    Login Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Login from './pages/Login'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+      <Login />
     </div>
   );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,19 @@
+'use-strict'
+import React from 'react';
+import logo from '../assets/logo.svg';
+import './Login.css'
+
+export default function Login() {
+  return (
+    <div className="login-container">
+      <form action="">
+        <img src={logo} alt="Logo Tindev"/>
+        <input
+          placeholder="digite se usuário no GitHib"
+        />
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  )
+}
+
