906cf6d2df22cf50d32384f8a74bc7af9de3f067
Dino Tudor <dinotudor@gmail.com>
2019-08-08 19:34:13 +0200

    add form and handleForm

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,25 @@
 'use-strict'
-import React from 'react';
+import React, { useState } from 'react';
+
 import logo from '../assets/logo.svg';
 import './Login.css'
 
 export default function Login() {
+  const [ username, setUsername ] = useState('');
+
+  function handelSubmit(e) {
+    e.preventDefault();
+    console.log(username)
+  }
+
   return (
     <div className="login-container">
-      <form>
+      <form onSubmit={handelSubmit}>
         <img src={logo} alt="Logo Tindev"/>
         <input
           placeholder="digite se usuário no GitHib"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
         />
         <button type="submit">Entrar</button>
       </form>
